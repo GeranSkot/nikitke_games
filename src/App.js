bc16316e9aa0e7d991393686d88c56167908f8dc
Aleksey Olshevsky <GeranSkot@gmail.com>
2021-02-27 19:15:56 +0300

    finalka

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+//Components and pages
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+//Styles
+import GlobalStyles from "./components/GlobalStyles";
+//Router
+import { Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello ignite</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalStyles />
+            <Nav />
+            <Route path={["/game/:id", "/"]}>
+                <Home />
+            </Route>
+        </div>
+    );
 }
 
 export default App;
